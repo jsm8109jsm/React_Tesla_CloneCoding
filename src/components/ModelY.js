0dd35a54910c19b9bc5e5aab2c8b49936a584227
jsm8109jsm <jsm8109jsm@gmail.com>
2022-06-22 21:50:11 +0900

    add: 휠 버튼

--- src/components/ModelY.js
+++ src/components/ModelY.js
@@ -11,16 +11,37 @@ export default function ModelY(){
     const [carImg, setCarImg] = useState('');
     const [color, setColor] = useState('white');
 
-    let wheelBtn;
+    const wheelBtnEl = document.getElementById('wheelBtn');
+
+    const [wheelName, setWheelName] = useState('');
+    const [wheelPrice, setWheelPrice] = useState('');
+
+    // let wheelBtn;
 
     let PerformUpgrade;
     useEffect(()=>{
         setOption1();
+        setCarImg('basic--white');
         setColorName('Pearl White Multi-Coat');
         setColorPrice('포함');
         setColor('white');
-        carImgName('white');
+        // setWheelName('19인치 제미니 휠');
+        // setWheelPrice('포함');
     },[])
+
+    window.onload = () => {
+        const buttonEl19 = document.getElementById('setWheelInfo19');
+        const buttonEl20 = document.getElementById('setWheelInfo20');
+        buttonEl19.addEventListener('click', () => {
+            setWheelName('19인치 제미니 휠');
+            setWheelPrice('포함');
+        })
+        
+        buttonEl20.addEventListener('click', () => {
+            setWheelName('20인치 인덕션 휠');
+            setWheelPrice('₩2,571,000');
+        })
+    }
 
     const setOption1 = () => {
         setDistance(511);
@@ -48,12 +69,17 @@ export default function ModelY(){
         setCarImg(prev => prev += '--' + newColor);
     }
 
+    
+
+
     if(!isPer){
         PerformUpgrade = '<div><h2 style="text-align: center; font-weight: normal; font-size: 1.75em; margin:0.5em;">퍼포먼스 업그레이드</h2><span style="display: block; text-align: center;">포함</span><ul><li style="margin-top: 0.5em">최고 속도를 217km/h에서 250km/h로 증가</li><li style="margin-top: 0.5em">21인치 Überturbine 휠</li><li style="margin-top: 0.5em">퍼포먼스 브레이크</li><li style="margin-top: 0.5em">낮아진 서스펜션</li><li style="margin-top: 0.5em">알루미늄 알로이 페달</li></ul></div>'
-        wheelBtn = '<button><img src="../images/21.png"/></button>';
+        // wheelBtn = '<button class="color--btn"><img src="../images/21.png" class="color--img"/></button>';
+        wheelBtnEl.innerHTML = '<button class="color--btn"><img src="../images/21.png" class="color--img"/></button>';
     }
     else{
-        wheelBtn = '<button><img src="../images/19.png"/></button><button><img src="../images/20.png"/></button>';
+        // wheelBtn = `<button class="color--btn" id="setWheelInfo19"><img src="../images/19.png" class="color--img"/></button><button class="color--btn" id="setWheelInfo20"><img src="../images/20.png" class="color--img"/></button>`;
+        wheelBtnEl.innerHTML = '<button class="color--btn" id="setWheelInfo19"><img src="../images/19.png" class="color--img"/></button><button class="color--btn" id="setWheelInfo20"><img src="../images/20.png" class="color--img"/></button>';
     }
 
     return(
@@ -84,45 +110,48 @@ export default function ModelY(){
                 <div className="color">
                     <h4 className="detail--title">색상</h4>
                         <div className="color--btn--div">
-                            <button className="color--btn" style={color==='white'?{border: '3px solid #3457b1'}:{border: '0px solid'}}><img src="../images/Paint_White.png" alt="white" className="color--img" onClick={()=>{
+                            <button className="color--btn" style={color==='white'?{border: '3px solid #3457b1'}:{border: '0px solid'}} onClick={()=>{
                                     setColorName('Pearl White Multi-Coat');
                                     setColorPrice('포함');
                                     setColor('white');
                                     carImgName('white');
                                 }
-                            }></img></button>
-                            <button className="color--btn" style={color==='black'?{border: '3px solid #3457b1'}:{border: '0px solid'}}><img src="../images/Paint_Black.png" alt="black" className="color--img" onClick={()=>{
+                            }><img src="../images/Paint_White.png" alt="white" className="color--img"></img></button>
+                            <button className="color--btn" style={color==='black'?{border: '3px solid #3457b1'}:{border: '0px solid'}} onClick={()=>{
                                     setColorName('Solid Black');
                                     setColorPrice('₩1,286,000');
                                     setColor('black');
                                     carImgName('black');
-                            }}></img></button>
-                            <button className="color--btn" style={color==='midnightsilver'?{border: '3px solid #3457b1'}:{border: '0px solid'}}><img src="../images/Paint_MidnightSilver.png" alt="midnightsilver" className="color--img" onClick={()=>{
+                            }}><img src="../images/Paint_Black.png" alt="black" className="color--img"></img></button>
+                            <button className="color--btn" style={color==='midnightsilver'?{border: '3px solid #3457b1'}:{border: '0px solid'}} onClick={()=>{
                                     setColorName('Midnight Silver Metallic');
                                     setColorPrice('₩1,286,000');
                                     setColor('midnightsilver');
                                     carImgName('midnightsilver');
-                            }}></img></button>
-                            <button className="color--btn" style={color==='blue'?{border: '3px solid #3457b1'}:{border: '0px solid'}}><img src="../images/Paint_Blue.png" alt="blue" className="color--img" onClick={()=>{
+                            }}><img src="../images/Paint_MidnightSilver.png" alt="midnightsilver" className="color--img"></img></button>
+                            <button className="color--btn" style={color==='blue'?{border: '3px solid #3457b1'}:{border: '0px solid'}} onClick={()=>{
                                     setColorName('Deep Blue Metallic');
                                     setColorPrice('₩1,286,000');
                                     setColor('blue');
                                     carImgName('blue');
-                            }}></img></button>
-                            <button className="color--btn" style={color==='red'?{border: '3px solid #3457b1'}:{border: '0px solid'}}><img src="../images/Paint_Red.png" alt="red" className="color--img" onClick={()=>{
+                            }}><img src="../images/Paint_Blue.png" alt="blue" className="color--img"></img></button>
+                            <button className="color--btn" style={color==='red'?{border: '3px solid #3457b1'}:{border: '0px solid'}} onClick={()=>{
                                     setColorName('Red Multi-Coat');
                                     setColorPrice('₩2,571,000');
                                     setColor('red');
                                     carImgName('red');
-                            }}></img></button>
+                            }}><img src="../images/Paint_Red.png" alt="red" className="color--img"></img></button>
                         </div>
                     <span className="color--name">{colorName}</span><span className="color--price">{colorPrice}</span>
                 </div>
 
                 <div className="color">
                     <h4 className="detail--title">휠</h4>
-                    <div dangerouslySetInnerHTML={{__html : wheelBtn}} className="performance"></div>
+                    <div id="wheelBtn" className="color--btn--div"></div>
+                    <span className="color--name">{wheelName}</span><span className="color--price">{wheelPrice}</span>
                 </div>
+
+
             </main>
         </div>
     )
